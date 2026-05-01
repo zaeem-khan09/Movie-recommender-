a8f7b656d9a302f18845f5ce24dae6c69589eac4
zaeem-khan09 <zaeem2398@gmail.com>
2026-05-01 06:54:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,52 +1,21 @@
 import streamlit as st
-import pickle
-import os
+import pandas as pd
 
-# Load data safely
-movies_path = os.path.join(os.getcwd(), "movies.pkl")
-similarity_path = os.path.join(os.getcwd(), "similarity.pkl")
+# Load dataset
+movies = pd.read_csv('movies.csv')
 
-movies = pickle.load(open(movies_path, 'rb'))
-similarity = pickle.load(open(similarity_path, 'rb'))
-
-# Default poster
-default_poster = "https://via.placeholder.com/150x220.png?text=Movie"
-
-# Recommend function
-def recommend(movie, num):
-    movie_index = movies[movies['title'] == movie].index[0]
-    distances = similarity[movie_index]
-    movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:num+1]
-
-    recommended_movies = []
-    for i in movies_list:
-        recommended_movies.append(movies.iloc[i[0]].title)
-
-    return recommended_movies
-
-# Page config
-st.set_page_config(page_title="Movie Recommender", layout="wide")
-
-# UI
 st.title("🎬 Movie Recommendation System")
-st.write("Find movies similar to your favorite ones!")
 
 selected_movie = st.selectbox(
     "Select a movie",
     movies['title'].values
 )
 
-num = st.slider("Number of recommendations", 5, 10)
-
-# Button
 if st.button("Recommend"):
-    with st.spinner("Finding best movies..."):
-        names = recommend(selected_movie, num)
+    st.success("Recommendations generated successfully!")
 
-        st.success("Recommendations generated successfully!")
+    # Simple recommendation (random 5 movies)
+    recommendations = movies.sample(5)
 
-        cols = st.columns(5)
-        for i in range(len(names)):
-            with cols[i % 5]:
-                st.image(default_poster)
-                st.caption(names[i])
+    for movie in recommendations['title']:
+        st.write(movie)
